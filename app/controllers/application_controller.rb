aa35a061f241c5a68b2c1e814755a7a498d3b8df
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-01-02 06:02:02 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,23 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    Article.create(params)
+    redirect "/articles/#{Article.last.id}"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
 end
